da394a91ba58fc1aeb6334421b17a74e0cef08a2
bryce <utbryceh@gmail.com>
2025-05-06 19:25:40 -0600

    thunar

--- config/nixosModules/bundles/desktop.nix
+++ config/nixosModules/bundles/desktop.nix
@@ -16,4 +16,5 @@ customModules.nixosBundle { inherit name config; } {
   nixosConfig.lf.enable = lib.mkDefault true;
   nixosConfig.git.enable = lib.mkDefault true;
   nixosConfig.fonts.enable = lib.mkDefault true;
+  nixosConfig.thunar.enable = lib.mkDefault true;
 }
