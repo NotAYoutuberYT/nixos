2100c81142a18e4416fbbd384551fc950e0aa233
bryce <utbryceh@gmail.com>
2025-06-06 23:14:13 -0600

    add icon fonts

--- config/homeManagerModules/bundles/desktop.nix
+++ config/homeManagerModules/bundles/desktop.nix
@@ -8,22 +8,15 @@
 {
   options.specialConfig.bundles.desktop.enable = lib.mkEnableOption "desktop bundle";
 
-  config = lib.mkIf config.specialConfig.bundles.desktop.enable {
-    specialConfig = {
-      xdg.enable = lib.mkDefault true;
+  config.specialConfig = lib.mkIf config.specialConfig.bundles.desktop.enable {
+    xdg.enable = lib.mkDefault true;
 
-      alacritty.enable = lib.mkDefault true;
-      vscodium.enable = lib.mkDefault true;
-      firefox.enable = lib.mkDefault true;
+    alacritty.enable = lib.mkDefault true;
+    vscodium.enable = lib.mkDefault true;
+    firefox.enable = lib.mkDefault true;
 
-      ncspot.enable = lib.mkDefault true;
+    ncspot.enable = lib.mkDefault true;
 
-      waybar.enable = lib.mkDefault true;
-    };
-
-    environment.systemPackages = [
-      pkgs.cm_unicode
-      pkgs.corefonts
-    ];
+    waybar.enable = lib.mkDefault true;
   };
 }
--- config/nixosModules/bundles/desktop.nix
+++ config/nixosModules/bundles/desktop.nix
@@ -20,6 +20,8 @@
 
     environment.systemPackages = with pkgs; [
       spotify
+      pkgs.cm_unicode
+      pkgs.corefonts
     ];
   };
 }
